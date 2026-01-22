cfbc7435ebf32c78dd1b027f26622772e9a00b2d
Sumit Pardeshi <sumitpardeshi1307@gmail.com>
2026-01-22 12:28:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///bus_ticketing.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ADMIN_CODE = 'ADMIN2024'  # Change this in production
+    ADMIN_CODE = 'ADMIN_ABC_25'  # Change this in production
